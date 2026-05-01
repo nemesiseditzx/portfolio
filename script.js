c19b500a9e6ba6f57d334c130a6ee9baedef9182
nemesiseditzx <nemesiseditzx984@gmail.com>
2026-04-30 20:12:55 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,37 @@
+// PARTICLES EFFECT
+const canvas = document.createElement("canvas");
+document.body.appendChild(canvas);
+
+const ctx = canvas.getContext("2d");
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+let particles = [];
+
+for (let i = 0; i < 100; i++) {
+  particles.push({
+    x: Math.random() * canvas.width,
+    y: Math.random() * canvas.height,
+    size: Math.random() * 2,
+    speed: Math.random() * 0.5
+  });
+}
+
+function animate() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+  particles.forEach(p => {
+    p.y += p.speed;
+    if (p.y > canvas.height) p.y = 0;
+
+    ctx.beginPath();
+    ctx.arc(p.x, p.y, p.size, 0, Math.PI * 2);
+    ctx.fillStyle = "#0ea5e9";
+    ctx.fill();
+  });
+
+  requestAnimationFrame(animate);
+}
+
+animate();
